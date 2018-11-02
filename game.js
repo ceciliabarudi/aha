136166a3ca7204e1164eb699bf7ced9fc51d934b
cecilia <blasterkat101@gmail.com>
2018-11-02 06:30:23 -0300

    create Game constructor and loop function

--- game.js
+++ game.js
@@ -1,3 +1,69 @@
 'use strict';
 //debugger;
 
+function Game(canvasElement) {
+	this.canvasElement = canvasElement;
+	this.ctx = this.canvasElement.getContext('2d');
+	this.gameIsOver = false;
+	this.player = null;
+	this.ideas = [];
+};
+
+Game.prototype.start = function() {
+	this.startLoop();
+}
+
+Game.prototype.startLoop = function() {
+	/*this.player = new Player(this.canvasElement);
+	this.ideas.push = (new Idea(this.canvasElement));
+	this.handleKeyPress = function(event) {
+			if (event.key === 'ArrowUp') {
+
+			} else if (event.key === 'ArrowDown') {
+
+			} else if(event.key === 'ArrowLeft') {
+
+			} else if (event.key === 'ArrowRight') {
+
+			}
+	}.bind(this);*/
+	
+	var timeoutID = setTimeout(function(){
+		this.gameIsOver = true;
+	}.bind(this), 2000);
+
+	var loop = function() {
+		if (!this.gameIsOver) {
+			requestAnimationFrame(loop);
+		} else {
+			this.finishGame();
+		}
+	}.bind(this);
+
+	loop();
+}
+
+Game.prototype.drawAll = function() {
+
+};
+
+Game.prototype.clearAll = function() {
+	//clear canvas
+	//filter ideas not on canvas from array
+};
+
+Game.prototype.updateAll = function() {
+
+};
+
+Game.prototype.checkAllCollisions = function() {
+
+};
+
+Game.prototype.gameIsOverCallback = function(callback) {
+	this.gameOverCallback = callback;
+};
+
+Game.prototype.finishGame = function() {
+	this.gameOverCallback();
+};
--- main.js
+++ main.js
@@ -2,98 +2,99 @@
 //debugger;
 
 function buildDOM(html) {
-    var div = document.createElement('div');
-    div.innerHTML = html;
-    return div.children[0];
+	var div = document.createElement('div');
+	div.innerHTML = html;
+	return div.children[0];
 };
 
 function main() {
 
-    //declare variables for states and interface elements
-    var splashScreen;
-    var gameScreen;
-    var gameOverScreen;
+	//declare variables for states and interface elements
+	var splashScreen;
+	var gameScreen;
+	var gameOverScreen;
 
-    var brainstormButton;
-    var rethinkButton;
-    var scoreElement;
-    var canvasElement;
-    var game;
+	var brainstormButton;
+	var rethinkButton;
+	var scoreElement;
+	var canvasElement;
+	var game;
 
-    
-    function buildSplash() {
-        splashScreen = buildDOM(`
-          <main>
-            <h1>Aha!</h1>
-            <h2>A Brainstorming Game</h2>
-            <button>brainstorm!</button>
-          </main>
-        `);
+	
+	function buildSplash() {
+		splashScreen = buildDOM(`
+			<main>
+				<h1>Aha!</h1>
+				<h2>A Brainstorming Game</h2>
+				<button>brainstorm!</button>
+			</main>
+		`);
 
-        document.body.prepend(splashScreen);
+		document.body.prepend(splashScreen);
 
-        brainstormButton = document.querySelector('button');
-        brainstormButton.addEventListener('click', destroySplash);
-    };
+		brainstormButton = document.querySelector('button');
+		brainstormButton.addEventListener('click', destroySplash);
+	};
 
-    function destroySplash() {
-        splashScreen.remove();
-        brainstormButton.removeEventListener('click', destroySplash);
-        buildGameScreen();
-    };
+	function destroySplash() {
+		splashScreen.remove();
+		brainstormButton.removeEventListener('click', destroySplash);
+		buildGameScreen();
+	};
 
-    function buildGameScreen() {
-        gameScreen = buildDOM(`
-        <main>
-          <p class="score">0</p>
-          <canvas></canvas>
-        </main>
-      `);
+	function buildGameScreen() {
+		gameScreen = buildDOM(`
+		<main>
+			<p class="score">0</p>
+			<canvas></canvas>
+		</main>
+		`);
 
-      document.body.prepend(gameScreen);
-      
-      canvasElement = document.querySelector('canvas');
-      //this shit won't work yet but i only realized after i wrote it so oh well
-      //scoreElement = document.querySelector('p.score'); won't change yet, i'm scared
-      //game = new Game(canvasElement);
-      //game.start();
-      //game.onGameOverCallback(destroyGameScreen);
-      var timeoutID = setTimeout(function(){
-        destroyGameScreen();
-      }.bind(this), 3000);
+		document.body.prepend(gameScreen);
+		
+		canvasElement = document.querySelector('canvas');
+		//this shit won't work yet but i only realized after i wrote it so oh well
+		//scoreElement = document.querySelector('p.score'); won't change yet, i'm scared
+		
+		game = new Game(canvasElement);
+		game.start();
+		game.gameIsOverCallback(destroyGameScreen);
+		
+		/*
+		var timeoutID = setTimeout(function(){
+			destroyGameScreen();
+		}.bind(this), 3000);
+		*/
+	};
 
-    };
+	function destroyGameScreen() {
+		console.log('I AM THE DESTROYER OF WORLDS');
+		gameScreen.remove();
+		buildGameOverScreen();
+	};
 
-    function destroyGameScreen() {
-      console.log('I AM THE DESTROYER OF WORLDS');
-      gameScreen.remove();
-      buildGameOverScreen();
-    };
+	function buildGameOverScreen() {
+		gameOverScreen = buildDOM(`
+			<main>
+				<h1>Done!</h1>
+				<h2>You came up with a Piece of Crap.</h2>
+				<button>rethink!</button>
+			</main>
+		`);
 
-    function buildGameOverScreen() {
-      console.log('TIS I, THE FRENCHIEST FRY');
-      gameOverScreen = buildDOM(`
-          <main>
-            <h1>Done!</h1>
-            <h2>You came up with a Piece of Crap.</h2>
-            <button>rethink!</button>
-          </main>
-        `);
+		document.body.prepend(gameOverScreen);
 
-        document.body.prepend(gameOverScreen);
+		rethinkButton = document.querySelector('button');
+		rethinkButton.addEventListener('click', destroyGameOverScreen);
+	};
 
-        rethinkButton = document.querySelector('button');
-        rethinkButton.addEventListener('click', destroyGameOverScreen);
+	function destroyGameOverScreen() {
+		gameOverScreen.remove();
+		rethinkButton.removeEventListener('click', destroyGameOverScreen);
+		buildGameScreen();
+	};
 
-    };
-
-    function destroyGameOverScreen() {
-      gameOverScreen.remove();
-      rethinkButton.removeEventListener('click', destroyGameOverScreen);
-      buildGameScreen();
-    };
-
-    buildSplash();
+	buildSplash();
 
 };
 
